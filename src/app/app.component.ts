4871f26b3667ebec426bdb872251b7474c989fb8
Zane Ives <deni23n@gmail.com>
2022-09-14 16:31:14 +0200

    updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tdf';
+  topics = ['Angular','React','Vue'];
+  equipment = [
+    {id:1,name:"",desc:"",isEquip:false},
+    {id:2,name:"",desc:"",isEquip:false},
+    {id:3,name:"",desc:"",isEquip:false},
+    {id:4,name:"",desc:"",isEquip:false},
+    {id:5,name:"",desc:"",isEquip:false},
+    {id:6,name:"",desc:"",isEquip:false},
+    {id:7,name:"",desc:"",isEquip:false},
+    {id:8,name:"",desc:"",isEquip:false},
+    {id:9,name:"",desc:"",isEquip:false},
+    {id:10,name:"",desc:"",isEquip:false},
+    {id:11,name:"",desc:"",isEquip:false},
+    {id:12,name:"",desc:"",isEquip:false},
+    {id:13,name:"",desc:"",isEquip:false},
+    {id:14,name:"",desc:"",isEquip:false},
+    {id:15,name:"",desc:"",isEquip:false},
+    {id:16,name:"",desc:"",isEquip:false},
+    {id:17,name:"",desc:"",isEquip:false},
+    {id:18,name:"",desc:"",isEquip:false},
+    {id:19,name:"",desc:"",isEquip:false},
+    {id:20,name:"",desc:"",isEquip:false}
+  ]
+
 }
